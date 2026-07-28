89ab30b14a57e0b5da6e7bc4c6a8bc211d2d4681
sparrowhawk425 <sparrowhawk425@gmail.com>
2026-07-27 17:09:50 -0700

    Adding linting to CI workflow

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import { handlerReadiness } from "./api/readiness.js";
 import { handlerNotesCreate, handlerNotesGet } from "./api/notes.js";
 import { handlerUsersCreate, handlerUsersGet } from "./api/users.js";
 
+function unused() {
+  // nada
+}
+
 const __dirname = path.resolve();
 
 if (!config.api.port) {
